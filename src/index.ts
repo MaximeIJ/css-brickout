9e90956968a43b90885ec1d87977d94e7940d1d0
Maxime Israel <5600516+MaximeIJ@users.noreply.github.com>
2023-10-21 16:08:22 -0400

    feat(package): export version

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export * from './game/Ball';
 export * from './game/Brick';
 export * from './game/Paddle';
 export * from './game/GameObject';
+export {version} from '../package.json';
 import './style.css';
